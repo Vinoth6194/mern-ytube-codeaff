4c54cabbd74679f4fab0cae5b8529d462f671275
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-07 21:56:46 +0530

    update api

--- controllers/postMessageController.js
+++ controllers/postMessageController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var router = express.Router();
+var ObjectID = require("mongoose").Types.ObjectId;
 
 var { PostMessage } = require("../models/postMessage");
 //*GET Api
@@ -29,5 +30,29 @@ router.post("/", (req, res) => {
       );
     }
   });
+});
+
+//*UPDATE Api
+router.put("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id : " + req.params.id);
+
+  var updatedRecord = {
+    title: req.body.title,
+    message: req.body.message,
+  };
+
+  PostMessage.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else
+        console.log(
+          "Error while updating a record : " + JSON.stringify(err, undefined, 2)
+        );
+    }
+  );
 });
 module.exports = router;
