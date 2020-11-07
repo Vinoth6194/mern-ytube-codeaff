f00262c6c49923ef6cf290417b752e4fcba34e07
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-07 21:54:13 +0530

    post api

--- controllers/postMessageController.js
+++ controllers/postMessageController.js
@@ -2,7 +2,7 @@ const express = require("express");
 var router = express.Router();
 
 var { PostMessage } = require("../models/postMessage");
-
+//*GET Api
 router.get("/", (req, res) => {
   PostMessage.find((err, docs) => {
     if (!err) {
@@ -14,5 +14,20 @@ router.get("/", (req, res) => {
     }
   });
 });
-
+//*POST Api
+router.post("/", (req, res) => {
+  var newRecord = new PostMessage({
+    title: req.body.title,
+    message: req.body.message,
+  });
+  newRecord.save((err, docs) => {
+    if (!err) {
+      res.send(docs);
+    } else {
+      console.log(
+        "Error in adding the record " + JSON.stringify(err, undefined, 2)
+      );
+    }
+  });
+});
 module.exports = router;
